d82a4907177ca7c81253966d1cad71a15a0729f0
User <ol.shink87@gmail.com>
2023-09-06 23:43:29 +0300

    @RequestMapping - общая часть запроса методов вынесена на уровне класса

--- src/main/java/pro/sky/java/course2/calculator/controller/Controller.java
+++ src/main/java/pro/sky/java/course2/calculator/controller/Controller.java
@@ -1,31 +1,33 @@
 package pro.sky.java.course2.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class Controller {
-    @GetMapping("/calculator")
+    @GetMapping
     public String sayWelcomeToCalc() {
         return "Добро пожаловать в клькулятор";
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     public String showSum(int num1, int num2) {
         return num1 + " + " + num2 + " = " + (num1 + num2);
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
     public String showSubtraction(int num1, int num2) {
         return num1 + " - " + num2 + " = " + (num1 - num2);
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     public String showMultiplication(int num1, int num2) {
         return num1 + " * " + num2 + " = " + (num1 * num2);
     }
 
-    @GetMapping("/calculator/divide")
+    @GetMapping("/divide")
     public String showDivision(int num1, int num2) {
         return num1 + " / " + num2 + " = " + ((float)num1 / num2);
     }
