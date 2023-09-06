938fc511b225d881529303069391891e83302014
User <ol.shink87@gmail.com>
2023-09-06 23:21:30 +0300

    method plus (/calculator/plus/)

--- src/main/java/pro/sky/java/course2/calculator/controller/Controller.java
+++ src/main/java/pro/sky/java/course2/calculator/controller/Controller.java
@@ -9,4 +9,9 @@ public class Controller {
     public String sayWelcomeToCalc() {
         return "Добро пожаловать в клькулятор";
     }
+
+    @GetMapping("/calculator/plus")
+    public String showSum(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
 }
