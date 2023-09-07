de1505c2228825dff1758bf255a7a6078981851f
User <ol.shink87@gmail.com>
2023-09-07 14:33:02 +0300

    ....

---
+++ src/main/java/pro/sky/java/course2/calculator/controller/CalculatorControllerAdvice.java
@@ -0,0 +1,14 @@
+package pro.sky.java.course2.calculator.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CalculatorControllerAdvice {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ошибка: " + ex.getMessage());
+    }
+}
