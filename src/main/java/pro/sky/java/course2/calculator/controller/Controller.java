5a6b2ee2bb1abfd3384a47f8df0ce18c297971a4
User <ol.shink87@gmail.com>
2023-09-07 11:27:09 +0300

    add ResponseEntity<String> + try-catch

--- src/main/java/pro/sky/java/course2/calculator/controller/Controller.java
+++ src/main/java/pro/sky/java/course2/calculator/controller/Controller.java
@@ -1,7 +1,10 @@
 package pro.sky.java.course2.calculator.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.java.course2.calculator.service.CalculatorService;
 
@@ -13,28 +16,35 @@ public class Controller {
     public Controller(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
+
     @GetMapping
     public String sayWelcomeToCalc() {
         return "Добро пожаловать в клькулятор";
     }
 
     @GetMapping("/plus")
-    public String showSum(int num1, int num2) {
+    public String showSum(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " + " + num2 + " = " + calculatorService.getSum(num1, num2);
     }
 
     @GetMapping("/minus")
-    public String showSubtraction(int num1, int num2) {
+    public String showSubtraction(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " - " + num2 + " = " + calculatorService.getSubtraction(num1, num2);
     }
 
     @GetMapping("/multiply")
-    public String showMultiplication(int num1, int num2) {
+    public String showMultiplication(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " * " + num2 + " = " + calculatorService.getMultiplication(num1, num2);
     }
 
     @GetMapping("/divide")
-    public String showDivision(int num1, int num2) {
-        return num1 + " / " + num2 + " = " + calculatorService.getDivision(num1, num2);
+    public ResponseEntity<String> showDivision(@RequestParam int num1, @RequestParam int num2) {
+        try {
+            float result = calculatorService.getDivision(num1, num2);
+            return ResponseEntity.ok(num1 + " / " + num2 + " = " + result);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ошибка: " + e.getMessage());
+        }
     }
 }
+
--- src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
@@ -1,6 +1,8 @@
 package pro.sky.java.course2.calculator.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
@@ -16,11 +18,16 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public int getMultiplication(int num1, int num2) {
-        return num1 * num2 ;
+        return num1 * num2;
     }
 
     @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public float getDivision(int num1, int num2) {
-        return num1/num2;
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Деление на ноль недопустимо");
+        } else {
+            return (float) num1 / num2;
+        }
     }
 }
